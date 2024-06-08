ddddabbf105fccd8e9c77352e22d899d37f80aaa
Deep88-lab <noobvalorant9@gmail.com>
2024-06-08 10:00:30 +0000

    datatypes

---
+++ .devcontainer/01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) using node js not browser
+
+console.log(3+3)
+
+let name="deep"
+let age=34
+let isLoggedIn=false
+
+//number = 2 to power of 53
+//bigint
+//string
+//boolean
+//null
+// undefined
+//symbol = unique
+
+
+
+//object 
+
+console.log(typeof age);
---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId="12453"
+let accountEmail="deep123@gmail.com"
+let accountCity="Delhi"
+var accountName="deep"
+//acountId="67483"
+accountCity="Malda"
+accountName="basunia"
+let accountState
+console.table([accountCity,accountEmail,accountId,accountName,accountState])
