1a8ecac6fa62a91a763c51df75a0717ba45b6ff6
Deep88-lab <noobvalorant9@gmail.com>
2024-06-09 03:30:08 +0000

    conversion of dataTypes

---
+++ .devcontainer/01_basics/conversion.js
@@ -0,0 +1,25 @@
+let score="34";
+console.log(typeof score)
+
+console.log(typeof (score))
+
+let num=Number (score);
+console.log(typeof num)
+
+//"33" => 33
+//"33abc" =? NaN
+//true =>1 ; false =>0
+
+let letLoggedIn=1;
+
+let booleanLoggedIn=Boolean (letLoggedIn)
+
+console.log(booleanLoggedIn);
+
+// 1=> true , 0= false
+//""=false , "deep"=>true
+
+let num1=66;
+let stringNumber=String (num)
+console.log(num1);
+console.log(typeof(stringNumber));
