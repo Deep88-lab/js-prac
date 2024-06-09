761fed8c7f5095d5844003e8a265f26ff481e59c
Deep88-lab <noobvalorant9@gmail.com>
2024-06-09 03:44:14 +0000

    operators

--- .devcontainer/01_basics/conversion.js
+++ .devcontainer/01_basics/conversion.js
@@ -1,25 +1,34 @@
-let score="34";
-console.log(typeof score)
+// let score="34";
+// console.log(typeof score)
 
-console.log(typeof (score))
+// console.log(typeof (score))
 
-let num=Number (score);
-console.log(typeof num)
+// let num=Number (score);
+// console.log(typeof num)
 
-//"33" => 33
-//"33abc" =? NaN
-//true =>1 ; false =>0
+// //"33" => 33
+// //"33abc" =? NaN
+// //true =>1 ; false =>0
 
-let letLoggedIn=1;
+// let letLoggedIn=1;
 
-let booleanLoggedIn=Boolean (letLoggedIn)
+// let booleanLoggedIn=Boolean (letLoggedIn)
 
-console.log(booleanLoggedIn);
+// console.log(booleanLoggedIn);
 
-// 1=> true , 0= false
-//""=false , "deep"=>true
+// // 1=> true , 0= false
+// //""=false , "deep"=>true
 
-let num1=66;
-let stringNumber=String (num)
-console.log(num1);
-console.log(typeof(stringNumber));
+// let num1=66;
+// let stringNumber=String (num)
+// console.log(num1);
+// console.log(typeof(stringNumber));
+
+// console.log(3+1);
+// console.log(3**3);
+// console.log(3%2)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(2+2+"4");
