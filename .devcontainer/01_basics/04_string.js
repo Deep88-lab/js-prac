2828ceaab7ea70fad0b40bec5f213d4556583226
Deep88-lab <noobvalorant9@gmail.com>
2024-06-12 16:18:29 +0000

    second attempt

--- .devcontainer/01_basics/04_string.js
+++ .devcontainer/01_basics/04_string.js
@@ -2,3 +2,22 @@ const name="Ravi"
 const id=89;
 console.log(name+id);
 console.log(`name of the employee is ${name} and his employee ID ${id}`)
+ let str1=new String("javascript")
+ console.log(str1.charAt(5))
+ console.log(str1.indexOf('t'));
+
+ const str2=str1.substring(0,5)
+ console.log(str2);
+
+ const str3=str1.slice(-9,5)
+ console.log(str3);
+
+ const newName="  Deep   "
+ console.log(newName);
+ console.log(newName.trim())
+
+ const url="https://hamiltonganj.com/deep@88basunia"
+
+ console.log(url.replace('@','#'))
+ console.log(url.includes("basunia"))
+ console.log(url.split('/'));
