b90c27dea7be76c03a5b3e716a810904f778f578
Deep88-lab <noobvalorant9@gmail.com>
2024-06-15 13:44:10 +0000

    dates

---
+++ .devcontainer/01_basics/07_dates.js
@@ -0,0 +1,15 @@
+let myDate=new Date
+// console.log(myDate.toDateString())
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString())
+// console.log(typeof myDate)
+
+//let currentDate=new Date(2024,2,11)
+let todayDate=new Date("10-11-2024")
+// console.log(todayDate.toDateString());
+
+let myTimeStamp=Date.now();
+// console.log(myTimeStamp)
+// console.log(todayDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
