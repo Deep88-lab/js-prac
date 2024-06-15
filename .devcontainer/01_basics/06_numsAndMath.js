e88e1e747ad52aa20fa550c110e22edff2e30087
Deep88-lab <noobvalorant9@gmail.com>
2024-06-15 13:22:43 +0000

    math and num

---
+++ .devcontainer/01_basics/06_numsAndMath.js
@@ -0,0 +1,32 @@
+const number=500
+// console.log(number);
+// console.log(typeof number);
+const balance=new Number(6000)
+console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+var num1=40;
+var num2=num1/6
+// console.log(num2.toFixed(2));
+
+const num3=53.556
+
+// console.log(num3.toPrecision(3));
+
+const num10=1000000
+// console.log(num10.toLocaleString('en-IN'))
+// console.log(Number.MAX_VALUE(12))
+ 
+
+
+//+++++++  math   ++++++++
+
+
+// console.log(Math.abs(-4))
+// console.log(Math.max(45,56,75,90));
+
+console.log(Math.floor((Math.random()*10)+1))
+const min=1
+const max=6;
+console.log(Math.floor(Math.random()*(max-min+1))+min);//imp 
